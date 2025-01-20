6b65d300436ab9c942911dfc1a1c47b01419c879
achmad-syidiq <achmadsyidiq@outlook.com>
2025-01-21 05:45:47 +0700

    refactor: extract header from App component into separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import TodoHeader from "./components/TodoHeader"
+
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-200 flex items-center justify-center font-mono">
       <div className="w-full max-w-2xl bg-gray-500 rounded-lg shadow-lg p-6">
-        <h1 className="text-center font-bold text-3xl text-gray-100">TodoList App</h1>
+        <TodoHeader>TodoList App</TodoHeader>
       </div>
     </div>
   )
